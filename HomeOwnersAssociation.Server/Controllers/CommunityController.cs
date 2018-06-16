7b412a1760a3c3d1a3ef692b35fb72083aa64539
Donovan Woodside <dwoodside@agbr.com>
2018-06-15 23:58:34 -0500

    Residence list

---
+++ HomeOwnersAssociation.Server/Controllers/CommunityController.cs
@@ -0,0 +1,25 @@
+﻿using HomeOwnersAssociation.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeOwnersAssociation.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class CommunityController : Controller
+    {
+      
+
+        [HttpGet("[action]")]
+        public Community Details()
+        {
+            return new Community
+            {
+                Id = Guid.NewGuid(),
+                Name = "Shadows of Sample River",
+            };
+        }
+    }
+}
